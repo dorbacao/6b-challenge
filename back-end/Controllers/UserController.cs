778acf251f22428a474cf5c7e41af973b939a74d
Marcus Dorbação <marcus.dorbacao@mindsource.pt>
2022-05-31 15:09:37 +0100

    Done: Sending a Jwt Token Middleware

--- back-end/Controllers/UserController.cs
+++ back-end/Controllers/UserController.cs
@@ -2,38 +2,24 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
-    /// <summary>
-    /// Controller is used to authenticate user by email and password
-    /// </summary>
-    [ApiController]
-    [Route("api/identity/user")]
-    [ApiExplorerSettings(GroupName = "identity/user")]
-    public class UserController : ControllerBase
-    {
-        private readonly ILogger<UserController> logger;
-        private readonly BookingDataContext dataContext;
+/// <summary>
+/// Controller is used to authenticate user by email and password
+/// </summary>
+[ApiController]
+[Route("api/identity/user")]
+[ApiExplorerSettings(GroupName = "identity/user")]
+public class UserController : ControllerBase
+{
+    private readonly ILogger<UserController> logger;
+    private readonly BookingDataContext dataContext;
     private readonly IJwtTokenService jwtTokenService;
 
     public UserController(ILogger<UserController> logger, BookingDataContext dataContext, IJwtTokenService jwtTokenService)
-        {
-            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            this.dataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
+    {
+        this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        this.dataContext = dataContext ?? throw new ArgumentNullException(nameof(dataContext));
         this.jwtTokenService = jwtTokenService ?? throw new ArgumentNullException(nameof(jwtTokenService));
     }
-
-        /// <summary>
-        /// Get All users
-        /// </summary>
-        /// <returns></returns>
-        [HttpGet]
-        [AllowAnonymous]
-        public async Task<IActionResult> GetAllUsersAsync()
-        {
-            var users = await dataContext.Set<User>().ToListAsync();
-
-            return Ok(users);
-
-        }
 
 
     /// <summary>
---
+++ back-end/Infraestructure/Aspnetcore/JwtMiddleware.cs
@@ -0,0 +1,29 @@
+﻿namespace SixB_Api.Infraestructure.Aspnetcore
+{
+    /// <summary>
+    /// Simple middleware to generate token
+    /// </summary>
+    public class JwtMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public JwtMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context, IJwtTokenService jwtUtils)
+        {
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+            var userId = jwtUtils.ValidateJwtToken(token);
+            if (userId != null)
+            {
+                // attach user to context on successful jwt validation
+                context.Items["UserId"] = userId;
+            }
+
+            await _next(context);
+        }
+    }
+
+}
